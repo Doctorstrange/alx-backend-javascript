5707bba78b36080f0b6de9f04287187e9df9d694
Doctorstrange <doctorstrange247@gmail.com>
2024-03-07 01:18:32 +0000

    nvm use 12.22.9

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  try {
-    Promise.all([uploadPhoto(), createUser()])
-      .then(([uploadResponse, userResponse]) => {
-        console.log(uploadResponse.body, userResponse.firstName, userResponse.lastName);
-      })
-      .catch(() => {
-        console.error('Signup system offline');
-      });
-  } catch (error) {
-    console.error('Signup system offline');
-  }
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
 
 export default handleProfileSignup;
