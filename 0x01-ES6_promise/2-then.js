edd1a5a784c5b479ecd50e3a0f010cf7d47a0a2c
Doctorstrange <doctorstrange247@gmail.com>
2024-03-07 00:41:12 +0000

    function handleResponseFromAPI(promise)

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,15 +2,13 @@ function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise
       .then(() => {
-        console.log('Got a response from the API');
         resolve({ status: 200, body: 'success' });
       })
       .catch(() => {
-        console.log('Got a response from the API (error)');
         reject(new Error());
       })
       .finally(() => {
-        // Logging already done in then/catch, so nothing to do here
+        console.log('Got a response from the API');
       });
   });
 }
