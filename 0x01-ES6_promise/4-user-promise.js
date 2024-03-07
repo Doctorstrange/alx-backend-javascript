22a8311e301d5146696a5209bb3401a39f243e48
Doctorstrange <doctorstrange247@gmail.com>
2024-03-07 01:42:33 +0000

    4-user-promise.js

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,11 @@
+function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    const user = {
+      firstName,
+      lastName,
+    };
+    resolve(user);
+  });
+}
+
+export default signUpUser;
