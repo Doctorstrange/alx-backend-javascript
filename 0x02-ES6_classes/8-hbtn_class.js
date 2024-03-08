17f26fcac59b7af30d505cf3f02d30c8e6103fe1
Doctorstrange <doctorstrange247@gmail.com>
2024-03-08 02:45:18 +0000

    8-hbtn_class.js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,38 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(size) {
+    if (typeof size === 'number') {
+      this._size = size;
+    } else {
+      throw new TypeError('Size must be a number');
+    }
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(location) {
+    if (typeof location === 'string') {
+      this._location = location;
+    } else {
+      throw new TypeError('Location must be a string');
+    }
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
