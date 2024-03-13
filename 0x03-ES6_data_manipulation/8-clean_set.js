7d69c41d9a8aaaf9b50b2f174a4507f762600d33
Doctorstrange <doctorstrange247@gmail.com>
2024-03-13 02:20:36 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(set, startString) {
-  const result = Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
-  return result;
+  if (startString) {
+    const result = Array.from(set)
+      .filter((value) => value.startsWith(startString))
+      .map((value) => value.slice(startString.length))
+      .join('-');
+    return result;
+  }
+  return '';
 }
