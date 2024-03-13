d6a6cd3069d35afa0e2fd9ec144a0cff894fe0e6
Doctorstrange <doctorstrange247@gmail.com>
2024-03-13 21:08:36 +0000

    ask_0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,41 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+  }
+  
+  const student1: Student = {
+    firstName: "peter",
+    lastName: "parker",
+    age: 18,
+    location: "New York",
+  };
+  
+  const student2: Student = {
+    firstName: "mary",
+    lastName: "jane",
+    age: 18,
+    location: "califonia",
+  };
+  
+  const studentsList: Student[] = [student1, student2];
+  
+  // Rendering the table using Vanilla JavaScript
+  const table = document.createElement("table");
+  const headerRow = table.insertRow();
+  const firstNameHeader = headerRow.insertCell(0);
+  firstNameHeader.textContent = "First Name";
+  const locationHeader = headerRow.insertCell(1);
+  locationHeader.textContent = "Location";
+  
+  studentsList.forEach((student) => {
+    const row = table.insertRow();
+    const firstNameCell = row.insertCell(0);
+    firstNameCell.textContent = student.firstName;
+    const locationCell = row.insertCell(1);
+    locationCell.textContent = student.location;
+  });
+  
+  document.body.appendChild(table);
+  
