15664db71946161cfe83b7f34829489e9b9000bd
Doctorstrange <doctorstrange247@gmail.com>
2024-03-14 00:53:03 +0000

    task2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,14 +8,28 @@ interface Teacher {
     [key: string]: any;
   }
   
-
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  
+  // Example usage:
   const teacher3: Teacher = {
     firstName: 'John',
     fullTimeEmployee: false,
     lastName: 'Doe',
     location: 'London',
-    contract: false
+    contract: false,
   };
   
+  const director1: Directors = {
+    firstName: 'Alice',
+    fullTimeEmployee: true,
+    lastName: 'Johnson',
+    location: 'New York',
+    yearsOfExperience: 10,
+    numberOfReports: 5,
+  };
 
+  
   console.log(teacher3);
+  console.log(director1);
